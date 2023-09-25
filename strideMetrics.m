46fb807d8654f4898d603efea50d78f6699e0404
Charles Guan <charles@ae.studio>
2023-09-25 15:31:08 -0700

    Add script to calculate stride metrics and save to file

---
+++ strideMetrics.m
@@ -0,0 +1,16 @@
+%STRIDEMETRICS Save out metriscs on Stride results
+addpath(genpath('intercomparison'));
+
+%% Modify data_path if necessary
+data_path = 'INTERCOMPARISON-PH1-DATA-V1.0';
+
+%% Calculate metrics for Stride
+BENCHMARK_NUM = 4;
+TRANSDUCER_NUM = 1;
+SIMULATOR = 'STRIDE';
+metrics = compareTwo(SIMULATOR, SIMULATOR, BENCHMARK_NUM, TRANSDUCER_NUM, data_path, true);
+
+%% Save out metrics as separate variables
+save_name = sprintf('metrics-BM%d-SC%d_%s.mat',...
+    BENCHMARK_NUM, TRANSDUCER_NUM, SIMULATOR);
+save(save_name, '-struct', 'metrics', '-v7.3');
